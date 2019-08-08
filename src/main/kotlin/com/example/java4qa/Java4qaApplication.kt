9ac4b23a6ebcf09ac505c94e6a50ff3df5946325
amullabaev <a.mullabaev@gmail.com>
2019-08-08 17:58:34 +0600

    initialized spring boot project

---
+++ src/main/kotlin/com/example/java4qa/Java4qaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.java4qa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Java4qaApplication
+
+fun main(args: Array<String>) {
+    runApplication<Java4qaApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/java4qa/Java4qaApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.example.java4qa
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Java4qaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
