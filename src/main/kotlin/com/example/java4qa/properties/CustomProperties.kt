cea3495cd8bd55ebe5f97901bf4f50ff6c51fabd
amullabaev <a.mullabaev@gmail.com>
2019-08-13 10:04:01 +0600

    created CustomProperties class for handling configuration properties

---
+++ src/main/kotlin/com/example/java4qa/properties/CustomProperties.kt
@@ -0,0 +1,8 @@
+package com.example.java4qa.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties("custom-properties")
+class CustomProperties {
+    lateinit var userName: String
+}
