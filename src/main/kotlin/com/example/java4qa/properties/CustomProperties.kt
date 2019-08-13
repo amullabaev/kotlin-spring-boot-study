6a7b2b0328b21092a1dbdd645a43f46a2c4768f0
amullabaev <a.mullabaev@gmail.com>
2019-08-13 13:13:48 +0600

    added lastname property in CustomProperties.kt

--- src/main/kotlin/com/example/java4qa/properties/CustomProperties.kt
+++ src/main/kotlin/com/example/java4qa/properties/CustomProperties.kt
@@ -4,5 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("custom-properties")
 class CustomProperties {
+
     lateinit var userName: String
+    lateinit var lastname: String
 }
