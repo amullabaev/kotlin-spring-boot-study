903a2f7bf454221967ce39002582300b938d5c3a
amullabaev <a.mullabaev@gmail.com>
2019-08-13 13:14:14 +0600

    added EnableConfigurationProperties annotation in main class

--- src/main/kotlin/com/example/java4qa/Java4qaApplication.kt
+++ src/main/kotlin/com/example/java4qa/Java4qaApplication.kt
@@ -1,9 +1,12 @@
 package com.example.java4qa
 
+import com.example.java4qa.properties.CustomProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomProperties::class)
 class Java4qaApplication
 
 fun main(args: Array<String>) {
